9e82d9227d2a6c97d3e005354c0072c99160605b
HunterBell512 <bellhunter512@gmail.com>
2023-11-13 20:47:08 -0500

    user options have been set up

--- script.js
+++ script.js
@@ -12,8 +12,29 @@ function writePassword() {
 
 // Function to generate a secure password
 function generatePassword () {
-  window.alert("Hello world!");
-  return 0;
+  let passwordLength = window.prompt("Enter a character length (8-128)"); // get password length
+  let expression;
+  let choicesSelected = [];
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8) {
+      window.alert("Chosen password length is too short!");
+    } else {
+      window.alert("Chosen password length is too long!");
+    }
+    passwordLength = window.prompt("Enter a character length (8-128)");
+  }
+
+  let includeLower = window.confirm("Do you want lowercase letters?");
+  let includeUpper = window.confirm("Do you want uppercase letters?");
+  let includeNumbers = window.confirm("Do you want numbers?");
+  let includeCharacters = window.confirm("Do you want special characters?");
+  console.log(includeLower);
+  console.log(includeUpper);
+  console.log(includeNumbers);
+  console.log(includeCharacters);
+
+  return passwordLength;
 }
 
 // Add event listener to generate button
